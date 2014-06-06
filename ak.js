5d56c777b2fa86462570fecfaf3984abce6f4766
oszo <oszo_oasis@hotmail.com>
2014-06-06 10:06:01 +0700

    Update ak.js

--- ak.js
+++ ak.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  var htmlPage = document.documentElement.innerHTML;
+  var findUSername = "var username = ";
+  var username='';
+  for (var index=htmlPage.search(findUSername)+findUSername.length;(htmlPage[index]!=';')&&(index<htmlPage.length);index++)
+  {
+    username+=htmlPage[index];
+  }
+  alert(username);
+}
